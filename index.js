21fd1d27df07a3b999728fd8f72a291248e7b624
PaulOrmaza <steven.ormaza816@ist17dejulio.edu.ec>
2024-12-02 09:21:16 -0500

    holamundoooo

---
+++ index.js
@@ -0,0 +1,24 @@
+//PROTOCOLO DE INTERCAMBIO
+var http=require('http');
+
+//CREANDO EL SERVIDOR MEDIANTE EL USO DE PROTOCOLO HTTP
+var server=http.createServer();
+
+
+// SERVICIO WEB 
+function mensaje(req,resp){
+    //ESTADO 200 SATISFACTORIO ,MENSAJE DE TIPO TEXTO PLANO CONFIGURADO MEDIANTE JSON
+    resp.writeHead(200,{'content-type':'text/plain'});
+    //ESCRIBO EL MENSAJE 
+    resp.write('HOLA MUNDO');
+    resp.end();
+
+
+}
+
+server.on('request',mensaje);
+server.listen(3000,function(){
+
+    console.log("LA SOLICITUD FUE REALIZADA MEDIANTE EL PUERTO 3000");
+
+});
